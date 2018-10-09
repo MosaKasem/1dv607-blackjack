dd05e8568234b971959ad0866f193cfe9d9d31b7
MosaKasem <thescorpion119@gmail.com>
2018-10-09 18:30:46 +0200

    moved stuff out.. it wouldn't run inside vs code

--- controller/PlayGame.cs
+++ controller/PlayGame.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace BlackJack.controller
+namespace controller
 {
     class PlayGame
     {
--- view/SwedishView.cs
+++ view/SwedishView.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace BlackJack.view
+namespace view
 {
     class SwedishView : IView 
     {
